fa41133baaee26a3389ca3e9169a23e9a02597fe
anandl-cuelogic <anand.lilhare@cuelogic.com>
2019-01-02 18:04:32 +0530

    Worked on Style and Props

--- albums/index.js
+++ albums/index.js
@@ -1,7 +1,10 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+// Import a library to help create a componet
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import Header from './src/components/header';
+// create a componet
+const App = () => (
+    <Header headerText={'Albums'} />
+  );
+//Render it to the device
+AppRegistry.registerComponent('albums', () => App);
