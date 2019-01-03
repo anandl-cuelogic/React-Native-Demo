e5b6202c279342960ad8318ab3ff9664211a85ea
anandl-cuelogic <anand.lilhare@cuelogic.com>
2019-01-03 18:47:10 +0530

    work on http request by react-native

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,14 @@
 // Import a library to help create a componet
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 // create a componet
 const App = () => (
+  <View>
     <Header headerText={'Albums'} />
+    <AlbumList />
+    </View>
   );
 //Render it to the device
 AppRegistry.registerComponent('albums', () => App);
