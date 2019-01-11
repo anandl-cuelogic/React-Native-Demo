ad660e2150c9225d45e92fcc70915f8a94a896b7
anandl-cuelogic <anand.lilhare@cuelogic.com>
2019-01-11 19:45:47 +0530

    work on redux

---
+++ tech_stack/src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducer';
+import { Header } from './componets/common';
+
+const App = () => {
+    return (
+        <Provider store={createStore(reducers)}>
+        <View>
+        <Header headerText="Tech Stack" />
+        </View>
+        </Provider>
+    );
+};
+export default App;
