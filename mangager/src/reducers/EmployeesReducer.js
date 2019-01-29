e009e466d8f1ea179bd31f83492ff7fe29be5796
anandl-cuelogic <anandl@cuelogic.com>
2019-01-29 10:15:49 +0530

    working on firebase database

--- mangager/src/reducers/EmployeesReducer.js
+++ mangager/src/reducers/EmployeesReducer.js
@@ -2,13 +2,13 @@ import {
     EMPLOYEE_FETCH_SUCCESS
 } from '../action/type';
 
+
 const INITIAL_STATE = {};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case EMPLOYEE_FETCH_SUCCESS:
-        console.log(action);
-        return state;
+        return action.payload;
         default:
         return state;
     }
